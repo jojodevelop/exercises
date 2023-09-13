49fc4bd2c11a7e20c733f9771243a1b4411eea49
niyomushumb <niyojosiane2001@gmail.com>
2023-09-13 02:27:45 -0700

    allexercises

---
+++ statements.js
@@ -0,0 +1,21 @@
+//control statement
+//=control statement+ loop statement
+//controll statement=if , if else, if else if ,switch case
+let year=5;
+if( year%4==0){
+    console.log('is divided by 4');
+}else{
+    console.log('the year is not divided by 4');
+}
+let score=88;
+if (score>=100&&score<=90){
+    console.log('grade A');}
+        else if (score<=89&&score>=80){
+    console.log('grade B');
+        }else if (score<=79&&score>=70){
+            console.log('grade C');
+             }else if (score<=79&&score>=70){
+            console.log('grade C');}
+        else{
+            console.log('grade D');}
+
